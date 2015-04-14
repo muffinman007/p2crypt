363f68817ed5a3960da5baa0a1f34dc6404cac75
muffinman007 <cs70falls218@hotmail.com>
2015-04-14 09:48:43 -0700

    Add SocketServer

---
+++ src/P2CNetwork/P2CNetwork/SocketServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2CNetwork 
+{
+	public class SocketServer 
+	{
+
+		#region Fields
+		#endregion Fields
+
+
+		#region Constructors
+		#endregion Constructors
+
+
+		#region Properties
+		#endregion Properties
+
+
+		#region Methods
+		#endregion Methods
+
+	}
+}
