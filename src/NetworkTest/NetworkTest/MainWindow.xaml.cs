d22e97ad7743224c26cf2db2ff57948d5a6502fa
muffinman007 <cs70falls218@hotmail.com>
2015-04-16 18:39:14 -0700

    Update NetworkTest

--- src/NetworkTest/NetworkTest/MainWindow.xaml.cs
+++ src/NetworkTest/NetworkTest/MainWindow.xaml.cs
@@ -1,202 +1,237 @@
-﻿#region Header
-
-/**
- * NOTE:
- *			- it is very important that the user supplied a user name.
- *
- *			- What you see on the GUI are the most important parts that are needed.
- *
- *			  After creating Server.ShakeHand methods , the program need to use the same port number across the board.
- *			- Suggestion: create a configurable setting for advance user to change the port number.
- *						  The port number is access by the Server Class before it starts up.
- *
- *			- I don't know how to access the delegate that is called when the window close so this code need to run when the window is closing:
- *					tokenSource.Cancel()
- *
- *			- Find a better way for user to disconnect the program from the net.
- *			  Maybe instead of creating the Server class is a singleton just let
- *			  MainWindow create a Server object.
- *
- *			- gracefully prevent user from connecting to localhost
- */
-
-#endregion Header
-
-
-
-using System;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using P2CCommon;
-using P2CNetwork;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+using Network;
 using P2CCore;
+using P2CCommon;
+using System.IO;
 
 namespace NetworkTest
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window, IUserInteractor
-	{
+	public partial class MainWindow:Window {
+
 		#region Fields
 
-		public readonly int defaultPort = 12345; // not the best way but it'll do for now. This number is access within Server class
+		bool hasServerStarted;
+		bool isWindowClosing;
 
-		CancellationTokenSource tokenSource; // to stop the Server gracefully if user x out of the app.
-		IUserAccount userAccount;
+		NetworkServer networkServer; 
+		UserAccount userAccount;
+
+		int messageSentCounter = 0;	
 
 		#endregion Fields
 
-		#region Constructors
-
-		public MainWindow()
-		{
+		public MainWindow() {
 			InitializeComponent();
-
-			for (int i = 0; i <= 255; ++i)
-			{							// populate each of the ip address section's drop down box
-				ipFirstByte.Items.Add(i);
-				ipSecondByte.Items.Add(i);
-				ipThirdByte.Items.Add(i);
-				ipFourthByte.Items.Add(i);
+			
+			for(int i = 0; i < 256; ++i){
+				cbFirstIP.Items.Add(i);
+				cbSecondIP.Items.Add(i);
+				cbThirdIP.Items.Add(i);
+				cbFourthIP.Items.Add(i);
 			}
 
-			// set the each of the ip address section to 0
-			ipFirstByte.SelectedIndex = 0;
-			ipSecondByte.SelectedIndex = 0;
-			ipThirdByte.SelectedIndex = 0;
-			ipFourthByte.SelectedIndex = 0;
+			cbFirstIP.SelectedIndex		= 0;
+			cbSecondIP.SelectedIndex	= 0;
+			cbThirdIP.SelectedIndex		= 0;
+			cbFourthIP.SelectedIndex	= 0;
 
-			txtChatWindow.IsReadOnly = true;
-			txtFriendsList.IsReadOnly = true;
+			hasServerStarted = false;
+			isWindowClosing = false;
 		}
 
-		#endregion Constructors
+		private void btnStart_Click(object sender, RoutedEventArgs e) {
+			if(userAccount == null)
+				userAccount = new UserAccount(){UserNick = txtNick.Text};
+			
+			if(networkServer == null){
+				if(String.IsNullOrEmpty(txtCustomPort.Text))
+					networkServer = new NetworkServer(userAccount.PublicProfile, lbHack); 
+				else
+					networkServer = new NetworkServer(userAccount.PublicProfile, lbHack, int.Parse(txtCustomPort.Text));
 
-		#region Methods
+				networkServer.P2CDS += new NetworkServer.P2CDeliveryService(PackageHandler);
+			}
 
-		private void btnDisconnect_Click(object sender, RoutedEventArgs e)
-		{
-			tokenSource.Cancel();
+			// we need to await this but most also give user a progress remote or a busy signal letting them know the network is starting up
+			networkServer.Start();			
 
-			Task.Factory.StartNew(() =>
-			{
-				Server.Instance.Disconnect();
-			});
+			btnStart.IsEnabled = false;
+			hasServerStarted = true;
+
+			btnStop.IsEnabled = true;
+			btnRemoteConnect.IsEnabled = true;
+			btnSend.IsEnabled = true;
+			btnChangeNick.IsEnabled = true;
+
+			txtStatus.Text = "Server running";
 		}
 
-		private void btnSend_Click_1(object sender, RoutedEventArgs e)
-		{
-			if (String.IsNullOrEmpty(txtMessage.Text) || String.IsNullOrWhiteSpace(txtMessage.Text))
+		private async void btnStop_Click(object sender, RoutedEventArgs e) {
+			networkServer.Disconnect();
+			hasServerStarted = false;
+
+			if(!isWindowClosing){
+				// notified everyone user logoff
+				Task.Factory.StartNew(()=>{ networkServer.Send(PackageStatus.LogOff, string.Empty); });
+
+				btnStart.IsEnabled = true;
+
+				btnStop.IsEnabled = false;
+				btnRemoteConnect.IsEnabled = false;
+				btnSend.IsEnabled = false;
+				btnChangeNick.IsEnabled = false;
+
+				txtStatus.Text = "Server stopped";
+			}
+			else{
+				// give time to send out notification
+				await LogoffNotificationAsync();
+			}
+		}
+
+		async Task LogoffNotificationAsync(){
+			await Task.Factory.StartNew(()=>{ networkServer.Send(PackageStatus.LogOff, string.Empty); });
+			return;
+		}
+
+
+		private void txtNick_TextChanged(object sender, TextChangedEventArgs e) {
+			// Require a nick before server can be started for the first time
+			if(String.IsNullOrEmpty(txtNick.Text) || String.IsNullOrWhiteSpace(txtNick.Text)){
+				if(!hasServerStarted)
+					btnSend.IsEnabled = false;
+				else
+					btnChangeNick.IsEnabled = false;
+			}
+			else{
+				if(!hasServerStarted)
+					btnStart.IsEnabled = true;
+				else
+					btnChangeNick.IsEnabled = true;
+			}
+		}
+
+		private async void btnSend_Click(object sender, RoutedEventArgs e) {
+			if(String.IsNullOrWhiteSpace(txtMessage.Text) || String.IsNullOrEmpty(txtMessage.Text))
 				return;
 
-			string msg = txtMessage.Text;
-			Task.Factory.StartNew(() => { Server.Instance.SendMessage(msg); });
+			String message = txtMessage.Text;
+
+			await Task.Factory.StartNew(()=>{ 
+				networkServer.Send(PackageStatus.Message, message); 
+			}).ConfigureAwait(false);
+
+			txtChatWindow.InvokeIfRequired(()=>{
+				txtChatWindow.AppendText(userAccount.UserNick + ":  " + txtMessage.Text + Environment.NewLine);
+				txtMessage.Clear();
+				txtMessage.Focus();
+				txtStatus.Text = ++messageSentCounter + " Message(s) sent";
+			});					
 		}
 
-		/// <summary>
-		/// This is how user will be able to connect to a destination ip.
-		/// Once a connection has been established user can start communicating with that ip address.
-		/// The ipaddress only need to connect once.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void btnShakeHand_Click(object sender, RoutedEventArgs e)
-		{
-			string str = ipFirstByte.SelectedItem.ToString() + "." +
-						 ipSecondByte.SelectedItem.ToString() + "." +
-						 ipThirdByte.SelectedItem.ToString() + "." +
-						 ipFourthByte.SelectedItem.ToString();
 
-			Task.Factory.StartNew(() =>
-			{
-				Server.Instance.ShakeHand(str);
+		async void PackageHandler(Package package){
+			string str = string.Empty;
+
+			if(package.PackageStatus == PackageStatus.LogOff || package.PackageStatus == PackageStatus.NickUpdate)
+				str = txtFriendsList.Text;
+
+			// some user may have thousands of nick on their list so it may be better to do the split on a different thread
+			Task.Factory.StartNew(()=>{
+				string[] nickArray = null;
+				if(!string.IsNullOrEmpty(str))
+					nickArray = str.Split(new string[]{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+				
+				switch(package.PackageStatus){
+					case PackageStatus.Connect:
+						txtChatWindow.InvokeIfRequired(()=>{
+							txtChatWindow.AppendText(package.PublicProfile.UserNick + " joined." + Environment.NewLine);
+							txtFriendsList.AppendText(package.PublicProfile.UserNick + Environment.NewLine);
+						});
+						break;
+
+					case PackageStatus.LogOff:
+						IEnumerable<string> listOfNicks = from nick in nickArray
+														  where !String.Equals(nick, package.Information.Item2)
+														  select nick;
+						txtChatWindow.InvokeIfRequired(()=>{
+							txtChatWindow.AppendText(package.Information.Item2 + " logged out." + Environment.NewLine);
+							txtFriendsList.Clear();
+							foreach(var nick in listOfNicks)
+								txtFriendsList.AppendText(nick + Environment.NewLine);
+						});
+						break;
+
+					case PackageStatus.NickUpdate:
+						for(int i = 0; i < nickArray.Length; ++i){
+							if(string.Equals(nickArray[i], package.Information.Item2)){
+								nickArray[i] = package.Information.Item3;
+								break;
+							}
+						}
+						txtChatWindow.InvokeIfRequired(()=>{
+							txtChatWindow.AppendText(package.Information.Item2 + " changed to " + package.Information.Item3 + Environment.NewLine);
+							txtFriendsList.Clear();
+							foreach(var nick in nickArray)
+								txtFriendsList.AppendText(nick + Environment.NewLine);
+						});
+						break;
+
+					case PackageStatus.Message:
+						string message = Encoding.UTF8.GetString(userAccount.Decrypt(package.Data));
+						txtChatWindow.InvokeIfRequired(()=>{
+							txtChatWindow.AppendText(package.Information.Item2 + ":  " + message + Environment.NewLine);
+						});
+						break;
+				}				
 			});
 		}
 
-		// the start button event
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-            
-			#region Start Server
-			tokenSource = new CancellationTokenSource();
-
-			// create a user account for the current user
-			userAccount = new UserAccount() { UserNick = userNickTxtBox.Text };
-
-			// feed server the data it want's and start it
-			Server.Initialization(userAccount, tokenSource, this);
-			Server.Instance.Start();
-			#endregion
-
-			// enable all the buttons
-			btnDisconnect.IsEnabled = true;
-			btnSend.IsEnabled = true;
-			btnShakeHand.IsEnabled = true;
-
-			// disable start button
-			btnStart.IsEnabled = false;
+		private void btnRemoteConnect_Click(object sender, RoutedEventArgs e) {
+			string ip = cbFirstIP.Text + "." + cbSecondIP.Text + "." + cbThirdIP.Text + "." + cbFourthIP.Text + ":" + txtPort.Text;
+			
+			Task.Factory.StartNew(()=>{
+				networkServer.ConnectToRemote(ip);
+			});
 		}
 
-		private void userNickTxtBox_TextChanged(object sender, TextChangedEventArgs e)
-		{
-			// The user have to enter in their user name before they can do anything
-			if (!(String.IsNullOrEmpty(userNickTxtBox.Text) || String.IsNullOrWhiteSpace(userNickTxtBox.Text)))
-			{
-				btnStart.IsEnabled = true;
-			}
-			else
-			{
-				btnStart.IsEnabled = false;
-			}
+		private void btnChangeNick_Click(object sender, RoutedEventArgs e) {
+			string oldNick = userAccount.UserNick;
+			userAccount.UserNick = txtNick.Text;
+
+			Task.Factory.StartNew(()=>{ networkServer.Send(PackageStatus.NickUpdate, oldNick); });
+			txtStatus.Text = "Nick change: " + oldNick + " to " + txtNick.Text;
 		}
 
-		#endregion Methods
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+			// hide the window giving it time to cleaning release resources in networkServer
+			this.Hide();
+			isWindowClosing = true;
+			btnStop_Click(this, null);
 
-        #region IUserInteractor
-        public void Notify(string information)
-        {
-            MessageBox.Show(information);
-        }
-        public void Notify(string text, string caption)
-        {
-            MessageBox.Show(text, caption);
-        }
-        public ITextList TextMessage { get { return new TextList(txtMessage); } }
-        public ITextList TextChatWindow { get { return new TextList(txtChatWindow); } }
-        public int DefaultPort { get { return defaultPort; } }
-        public ITextList TextFriendsList { get { return new TextList(txtFriendsList); } }
+			networkServer.P2CDS -= PackageHandler;
+			networkServer.Exit();
+			networkServer = null;
+		}
 
-        #endregion
+	}
 
-        public class TextList : ITextList
-        {
-            private TextBox textBox;
-
-            public TextList(TextBox tb)
-            {
-                textBox = tb;
-            }
-
-            public void AppendText(string text)
-            {
-                textBox.InvokedIfRequired(() => textBox.AppendText(text));
-            }
-            public void Clear()
-            {
-                textBox.InvokedIfRequired(() => textBox.Clear());
-            }
-            public string Text
-            {
-                get
-                {
-                    string buff = "";
-                    textBox.InvokedIfRequired(() => buff = textBox.Text);
-                    return buff;
-                }
-            }
-        }
-    }
 }
